c1c52292135133e85ec97110ad8553b3a54aa5bd
Chiara Diaz <diazchiara.1998@gmail.com>
2026-06-25 22:54:59 -0300

    TPI-Programacion2-Diaz-Chiara

---
+++ ProyectoFinal/CategoriaService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+/**
+ *
+ * @author Chiara Diaz
+ */
+public class EntidadNoEncontradaException extends Exception {
+    
+
+
+
+
+    public EntidadNoEncontradaException(String mensaje) {
+        super(mensaje);
+    }
+}
+    
+
